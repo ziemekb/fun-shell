69278a702562411502daa23a72cb84915342e01b
Ziemek <ziemowit.baczewski@gmail.com>
2024-01-19 12:44:47 +0100

    add mainLoop and terminating with eof

--- Main.hs
+++ Main.hs
@@ -1,16 +1,40 @@
+import Control.Monad
+import System.IO
+import Data.Char
 
 prompt :: IO ()
 prompt = putStr "#> "
 
+{--
 -- Here lexical tokenization takes place
 parse :: String -> [t]
-parse s = []
+parse s = concatMap 
 
 -- Here interpretation of the token or some abstract syntax
 interpret :: [t] -> ()
 interpret a = ()
+--}
+
+readLine :: IO (String)
+readLine = 
+        do 
+            input <- getLine
+            return $ map (\c -> if isSpace c then '\0' else c) input
+
+mainLoop :: IO ()
+mainLoop = do
+        prompt
+        eof <- isEOF
+        if eof 
+            then return () 
+            else do 
+                input <- readLine
+                putStrLn input
+                mainLoop
 
 main :: IO ()
-main = do prompt
-          input <- getLine
-          main
+main = do 
+        hSetBuffering stdin LineBuffering
+        tdevin  <- hIsTerminalDevice stdin
+        tdevout <- hIsTerminalDevice stdout
+        if tdevin && tdevout then mainLoop else return ()
