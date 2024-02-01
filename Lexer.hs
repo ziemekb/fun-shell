1f42ac82e6c3e24dfb83a2dca7fbee72c4fe2476
Ziemek <ziemowit.baczewski@gmail.com>
2024-02-01 04:25:49 +0100

    change data types in Lexer add splitAndRemoveDelimiter

--- Lexer.hs
+++ Lexer.hs
@@ -1,21 +1,17 @@
 module Lexer where
+import Data.List (groupBy)
 
-data Token = NULL | AND | OR | PIPE | BGJOB | COMMAND [String]
-    deriving (Show)
+type Background    = Bool
+type SingleCommand = [String]
+type Pipeline      = [SingleCommand]
+type Command       = Either SingleCommand Pipeline
 
-tokenize :: String -> [Token]
-tokenize line =
-    strToTok (words line) []
+splitByDelimiter :: Eq a => a -> [a] -> [[a]]
+splitByDelimiter delimiter = filter (not . null) . groupBy (\x y -> y /= delimiter) 
 
-strToTok :: [String] -> [String] -> [Token]
-strToTok [] []            = []
-strToTok [] acc           = [COMMAND (reverse acc)]
-strToTok ("&&": args) []  = AND   : strToTok args []
-strToTok ("||": args) []  = OR    : strToTok args []
-strToTok ("|" : args) []  = PIPE  : strToTok args []
-strToTok ("&" : args) []  = BGJOB : strToTok args []
-strToTok ("&&": args) acc = COMMAND (reverse acc) : AND   : strToTok args []
-strToTok ("||": args) acc = COMMAND (reverse acc) : OR    : strToTok args []
-strToTok ("|" : args) acc = COMMAND (reverse acc) : PIPE  : strToTok args []
-strToTok ("&" : args) acc = COMMAND (reverse acc) : BGJOB : strToTok args []
-strToTok (cmd : args) acc = strToTok args $ cmd : acc
+removeDelimiter :: Eq a => a -> [[a]] -> [[a]]
+removeDelimiter delimiter = map (filter (/= delimiter))
+
+splitAndRemoveDelimiter :: Eq a => a -> [a] -> [[a]]
+splitAndRemoveDelimiter delimiter = removeDelimiter delimiter . splitByDelimiter delimiter
+
