488f8ad23437682fc81e1e6e5ffafd94367fbcd9
Ziemek <ziemowit.baczewski@gmail.com>
2024-02-01 04:44:08 +0100

    add tokenization in accordance to the new data types

--- Lexer.hs
+++ Lexer.hs
@@ -5,6 +5,9 @@ type Background    = Bool
 type SingleCommand = [String]
 type Pipeline      = [SingleCommand]
 type Command       = Either SingleCommand Pipeline
+-- type FilePath = String  
+-- data Redirection = (Input FilePath) Command  (Output FilePath) |
+--                    (Input FilePath) Pipeline (Output FilePath)
 
 splitByDelimiter :: Eq a => a -> [a] -> [[a]]
 splitByDelimiter delimiter = filter (not . null) . groupBy (\x y -> y /= delimiter) 
@@ -15,3 +18,17 @@ removeDelimiter delimiter = map (filter (/= delimiter))
 splitAndRemoveDelimiter :: Eq a => a -> [a] -> [[a]]
 splitAndRemoveDelimiter delimiter = removeDelimiter delimiter . splitByDelimiter delimiter
 
+isPipeline :: [String] -> Bool
+isPipeline = elem "|" 
+
+tokenize :: String -> (Command, Background)
+tokenize line 
+    | bg        = (parser $ init toks, bg)
+    | otherwise = (parser toks, bg)
+    where toks = words line
+          bg   = last toks == "&"
+
+parser :: [String] -> Command
+parser words 
+    | isPipeline words = Right $ splitAndRemoveDelimiter "|" words
+    | otherwise        = Left words   
